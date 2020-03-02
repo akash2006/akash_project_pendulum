1c78a19cd6f277b7d76e2dd75591f3ed974c06ef
Akash Maity <59837849+akash2006@users.noreply.github.com>
2020-03-02 17:54:04 +0530

    Update String.js

--- String.js
+++ String.js
@@ -3,17 +3,17 @@ class String {
         var options = {
             bodyA:bodyA,
             pointB:pointB,
-            stiffness:1,
+            stiffness:0.1,
             length:150
         }
-        
+        this.pointB = pointB;
         this.string = Constraint.create(options);
         World.add(world,this.string);
     }
     display(){
         var pointA = this.string.bodyA.position;
-        var pointB = pointB;
-        
+        var pointB = this.pointB;
+        stroke("black");
         strokeWeight(7);
         line(pointB.x,pointB.y,pointA.x,pointA.y);
     }
